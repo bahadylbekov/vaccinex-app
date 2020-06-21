3695d8819bc07a8d56b45de4e875cf101350af2f
bahadylbekov <33404905+bahadylbekov@users.noreply.github.com>
2020-06-21 07:14:27 +0300

    add: viruses, organizations page layout, changed profile layout

--- src/app/organizations/organizations.component.ts
+++ src/app/organizations/organizations.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Organization {
+  id: string;
+  name: string;
+  city: string;
+  country: string;
+}
+
 @Component({
   selector: 'app-organizations',
   templateUrl: './organizations.component.html',
@@ -8,6 +15,11 @@ import { Component, OnInit } from '@angular/core';
 export class OrganizationsComponent implements OnInit {
 
   constructor() { }
+  organizations: Organization[] = [
+    {id: 'nusvdni0w1', name: 'Stanford Medical Research', city: 'San Francisco', country: 'USA'},
+    {id: 'nusvdni0w2', name: 'UC Berkeley Medical Institute', city: 'Berkeley', country: 'USA'},
+    {id: 'nusvdni0w3', name: 'Yale Health', city: 'New Haven', country: 'USA'},
+  ]
 
   ngOnInit(): void {
   }
--- src/app/viruses/viruses.component.ts
+++ src/app/viruses/viruses.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Virus {
+  id: string;
+  name: string;
+  family: string;
+}
+
 @Component({
   selector: 'app-viruses',
   templateUrl: './viruses.component.html',
@@ -8,6 +14,14 @@ import { Component, OnInit } from '@angular/core';
 export class VirusesComponent implements OnInit {
 
   constructor() { }
+  viruses: Virus[] = [
+    {id: 'nusvdni0w1', name: 'SARS-CoV-2', family:'Coronaviridae'},
+    {id: 'nusvdni0w9', name: 'HIV/AIDS', family: 'Retroviridae'},
+    {id: 'nusvdni0w7', name: 'Influenza', family: 'Retroviridae',},
+    {id: 'nusvdni0w6', name: 'Hepatitis C', family: 'Flaviviridae',},
+    {id: 'nusvdni0w5', name: 'Astrovirus', family: 'Astroviridae',},
+    {id: 'nusvdni0w4', name: 'Ebola virus', family: 'Filoviridae',}
+  ]
 
   ngOnInit(): void {
   }
