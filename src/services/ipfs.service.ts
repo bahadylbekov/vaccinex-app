233fd1c71bba5ba5dc774d5e763689ad37090f8d
bahadylbekov <33404905+bahadylbekov@users.noreply.github.com>
2020-06-22 05:25:38 +0300

    add: ipfs service methods for uploading and getting data

--- src/services/ipfs.service.ts
+++ src/services/ipfs.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import * as IPFS from 'ipfs-mini' 
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class IpfsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+  public async UploadData(data: string): Promise<any> {
+    await this.ipfs.add(data)
+      .then(result => console.log(result))
+      .catch(error => console.log(error));
+  }
+
+  public async GetData(path: string): Promise<any> {
+    await this.ipfs.cat(path, (err, result) => {
+      console.log(err, result);
+      return result
+    });
+  }
 }
